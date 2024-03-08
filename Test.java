33ae2c49beed932a98cfe3b896132efde19ef76f
rahullaha <rahulkumarlaha40@gmail.com>
2024-03-09 01:19:53 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+class Test{
+	private static void add(String s){
+		System.out.println(s+" welcome to the course");
+	}
+	public static void main(String[] args){
+		System.out.println("Welcome to Java Dev course");
+		add("Rahul");
+	}
+}
